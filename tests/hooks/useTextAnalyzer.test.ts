ac3628e097f12eb24807a95b97111f632144f236
Invulner <invulner91@gmail.com>
2024-03-31 19:43:06 +0200

    Update tests for useTextAnalyzer

--- tests/hooks/useTextAnalyzer.test.ts
+++ tests/hooks/useTextAnalyzer.test.ts
@@ -3,133 +3,99 @@ import { renderHook } from '@testing-library/react-hooks';
 import useTextAnalyzer from '../../src/useTextAnalyzer';
 
 describe('useTextAnalyzer', () => {
-  it('should handle empty input correctly', () => {
-    const { result } = renderHook(() => useTextAnalyzer({ text: '' }));
+  const text = `
+    This is the first paragraph. It's SeCoNd contains multiple words.
+
+    This is the second paragraph! it Contains numbers like 123 and special characters like @#$.
+
+    This iS the third paragraph? It includes emojis like 😀 and unicode characters like éñö.
+
+    This is the fourth paragraph. It's followed by a paragraph.
+
+    This iS tHe fifth Paragraph. It contains whitespace characters like \t and \r.
+
+    This is the sixth Paragraph. It also includes non-alphanumeric characters: %^&*()_+-=[]{}|;:'",<>/\`~.
+
+    This is the seventh paragraph. It's shorter.
+`;
+
+  it('should analyze text correctly with searchTerm and when ignoreCase is true', () => {
+    const { result } = renderHook(() => useTextAnalyzer({ text: text.trim(), searchTerm: 'SeCoNd', ignoreCase: true }));
 
     expect(result.current).toEqual({
-      wordCount: 0,
-      charCount: 0,
-      sentenceCount: 0,
-      paragraphCount: 0,
+      wordCount: 80,
+      charCount: 560,
+      sentenceCount: 14,
+      paragraphCount: 7,
+      searchFrequency: 2,
+      readingTime: 24,
+      mostFrequentWord: 'paragraph',
+      leastFrequentWord: 'first',
+      mostFrequentCharacter: 'a',
+      leastFrequentCharacter: '!',
+    });
+  });
+
+  it('should analyze text correctly with searchTerm and when ignoreCase is false', () => {
+    const { result } = renderHook(() =>
+      useTextAnalyzer({
+        text: text.trim(),
+        searchTerm: 'SeCoNd',
+        ignoreCase: false,
+      }),
+    );
+
+    expect(result.current).toEqual({
+      wordCount: 80,
+      charCount: 560,
+      sentenceCount: 14,
+      paragraphCount: 7,
+      searchFrequency: 1,
+      readingTime: 24,
+      mostFrequentWord: 'This',
+      leastFrequentWord: 'first',
+      mostFrequentCharacter: 'a',
+      leastFrequentCharacter: 'N',
+    });
+  });
+
+  it('should analyze text correctly with trimText', () => {
+    const { result } = renderHook(() => useTextAnalyzer({ text, trimText: false }));
+
+    expect(result.current).toEqual({
+      wordCount: 80,
+      charCount: 566,
+      sentenceCount: 14,
+      paragraphCount: 7,
       searchFrequency: 0,
-      readingTime: 0,
-      mostFrequentWord: '',
-      leastFrequentWord: '',
-      mostFrequentCharacter: '',
-      leastFrequentCharacter: '',
-    });
-  });
-
-  it('should handle text parameter with multiple paragraphs and sentences with whitespace variations', () => {
-    const text = '   This is a  test.   This is another test.\n    This is a third paragraph.    ';
-    const { result } = renderHook(() => useTextAnalyzer({ text }));
-
-    expect(result.current).toEqual({
-      wordCount: 13,
-      charCount: 71,
-      sentenceCount: 3,
-      paragraphCount: 2,
-      searchFrequency: 0,
-      readingTime: 4,
-      mostFrequentWord: 'this',
-      leastFrequentWord: 'another',
-      mostFrequentCharacter: 't',
-      leastFrequentCharacter: 'n',
-    });
-  });
-
-  it('should handle text parameter with multiple paragraphs and sentences with searchTerm', () => {
-    const text = `  This iS the first  paragraph.    It has multiple sentences.
-    this is the second paragraph. It also has multiple sentences.    `;
-    const { result } = renderHook(() => useTextAnalyzer({ text, searchTerm: 'second' }));
-
-    expect(result.current).toEqual({
-      wordCount: 19,
-      charCount: 125,
-      sentenceCount: 4,
-      paragraphCount: 2,
-      searchFrequency: 1,
-      readingTime: 6,
-      mostFrequentWord: 'this',
+      readingTime: 24,
+      mostFrequentWord: 'paragraph',
       leastFrequentWord: 'first',
-      mostFrequentCharacter: 's',
-      leastFrequentCharacter: 'f',
+      mostFrequentCharacter: 'a',
+      leastFrequentCharacter: '!',
     });
   });
 
-  it('should handle text parameter with multiple paragraphs and sentences with searchTerm when ignoreCase is true', () => {
-    const text = `  This iS the first  paragraph.    It has multiple sentences.
-    this is the second paragraph. It also has multiple sentences.    `;
-    const { result } = renderHook(() => useTextAnalyzer({ text, searchTerm: 'second', ignoreCase: true }));
+  it('should analyze text correctly with searchTerm containing special characters', () => {
+    const { result } = renderHook(() =>
+      useTextAnalyzer({
+        text: text.trim(),
+        searchTerm: '@#$',
+        ignoreCase: true,
+      }),
+    );
 
     expect(result.current).toEqual({
-      wordCount: 19,
-      charCount: 125,
-      sentenceCount: 4,
-      paragraphCount: 2,
+      wordCount: 80,
+      charCount: 560,
+      sentenceCount: 14,
+      paragraphCount: 7,
       searchFrequency: 1,
-      readingTime: 6,
-      mostFrequentWord: 'this',
+      readingTime: 24,
+      mostFrequentWord: 'paragraph',
       leastFrequentWord: 'first',
-      mostFrequentCharacter: 's',
-      leastFrequentCharacter: 'f',
-    });
-  });
-
-  it('should handle text parameter with multiple paragraphs and sentences with searchTerm when ignoreCase is false', () => {
-    const text = `  This iS the first  paragraph.    It has multiple sentences.
-    this is the second paragraph. It also has multiple sentences.    `;
-    const { result } = renderHook(() => useTextAnalyzer({ text, searchTerm: 'second', ignoreCase: false }));
-
-    expect(result.current).toEqual({
-      wordCount: 19,
-      charCount: 125,
-      sentenceCount: 4,
-      paragraphCount: 2,
-      searchFrequency: 1,
-      readingTime: 6,
-      mostFrequentWord: 'the',
-      leastFrequentWord: 'This',
-      mostFrequentCharacter: 's',
-      leastFrequentCharacter: 'T',
-    });
-  });
-
-  it('should handle text parameter with multiple paragraphs and sentences with searchTerm when trimText is true', () => {
-    const text = `  This iS the first  paragraph.    It has multiple sentences.
-    this is the second paragraph. It also has multiple sentences.    `;
-    const { result } = renderHook(() => useTextAnalyzer({ text, searchTerm: 'second', trimText: true }));
-
-    expect(result.current).toEqual({
-      wordCount: 19,
-      charCount: 125,
-      sentenceCount: 4,
-      paragraphCount: 2,
-      searchFrequency: 1,
-      readingTime: 6,
-      mostFrequentWord: 'this',
-      leastFrequentWord: 'first',
-      mostFrequentCharacter: 's',
-      leastFrequentCharacter: 'f',
-    });
-  });
-
-  it('should handle text parameter with multiple paragraphs and sentences with searchTerm when trimText is false', () => {
-    const text = `  This iS the first  paragraph.    It has multiple sentences.
-    this is the second paragraph. It also has multiple sentences.    `;
-    const { result } = renderHook(() => useTextAnalyzer({ text, searchTerm: 'second', trimText: false }));
-
-    expect(result.current).toEqual({
-      wordCount: 19,
-      charCount: 131,
-      sentenceCount: 4,
-      paragraphCount: 2,
-      searchFrequency: 1,
-      readingTime: 6,
-      mostFrequentWord: 'this',
-      leastFrequentWord: 'first',
-      mostFrequentCharacter: 's',
-      leastFrequentCharacter: 'f',
+      mostFrequentCharacter: 'a',
+      leastFrequentCharacter: '!',
     });
   });
 });
