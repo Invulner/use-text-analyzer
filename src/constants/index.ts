e782e4e3bbac1b50c4a878ffd14b2646072f09d0
Invulner <invulner91@gmail.com>
2024-04-09 21:24:12 +0200

    Store default value for reading speed in constants

---
+++ src/constants/index.ts
@@ -0,0 +1,2 @@
+// Assuming average reading speed of 200 words per minute
+export const WORDS_PER_MINUTE = 200;
--- src/utils/estimateReadingTime.ts
+++ src/utils/estimateReadingTime.ts
@@ -1,11 +1,10 @@
+import { WORDS_PER_MINUTE } from '../constants';
+
 /**
  * Estimates the reading time of the text in seconds.
  * @param {number} wordCount - The number of words in the text.
  * @returns {number} The estimated reading time of the text in seconds.
  */
 export function estimateReadingTime(wordCount: number): number {
-  // Assuming average reading speed of 200 words per minute
-  const wordsPerMinute = 200;
-
-  return Math.ceil(wordCount / (wordsPerMinute / 60));
+  return Math.ceil(wordCount / (WORDS_PER_MINUTE / 60));
 }
